5bc80f5025fae9e1376033a00f58261a9c36d8f1
Sarthak Jain <332Sarthak22@gmail.com>
2022-06-15 01:27:43 +0530

    "max subarray ques"

---
+++ MaxSubarray.java
@@ -0,0 +1,22 @@
+public class MaxSubarray {
+    public int maxSubarray(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int prev = nums[0];
+        int max = prev;
+        for (int i = 1; i < nums.length; i++) {
+            prev = Math.max(nums[i], prev + nums[i]);
+            max = Math.max(max, prev);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int[] arr2 = { -2, -5, -3, -6 };
+        MaxSubarray m = new MaxSubarray();
+        System.out.println(m.maxSubarray(arr1)); // should output -2
+        System.out.println(m.maxSubarray(arr2)); // should output 6
+    }
+}
