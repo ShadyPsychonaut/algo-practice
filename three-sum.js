708e0fed3770c4f5be50b26bec4c985dbeb36643
Sarthak Jain <332Sarthak22@gmail.com>
2022-06-20 03:09:48 +0530

    modified 3sum

--- three-sum.js
+++ three-sum.js
@@ -7,19 +7,7 @@ const threeSum = (nums) => {
     const sumOfThree = sumOfTwo.map((way) => [nums[i], ...way]);
     for (let j = 0; j < sumOfThree.length; j++) {
       const ans = sumOfThree[j];
-      if (
-        result
-          .map((solution) =>
-            ans.every(
-              (val) =>
-                solution.includes(val) &&
-                ans.filter((el) => el === val).length <=
-                  solution.filter((el) => el === val).length
-            )
-          )
-          .includes(true)
-      )
-        continue;
+      if (exists(result, ans)) continue;
       result.push(ans);
     }
   }
@@ -34,23 +22,26 @@ const twoSum = (target, nums, pos) => {
     const newTarget = target - nums[i];
     if (newTarget in dict) {
       const sumOfTwo = [newTarget, nums[i]];
-      if (
-        !result
-          .map((sol) =>
-            sumOfTwo.every(
-              (val) =>
-                sol.includes(val) &&
-                sumOfTwo.filter((el) => el === val).length <=
-                  sol.filter((el) => el === val).length
-            )
-          )
-          .includes(true)
-      )
-        result.push(sumOfTwo);
+      if (!exists(result, sumOfTwo)) result.push(sumOfTwo);
     }
     dict[nums[i]] = i;
   }
   return result;
+};
+
+const exists = (result, toCheck) => {
+  const ex = result.map((sol) =>
+    toCheck.every(
+      (val) =>
+        sol.includes(val) &&
+        toCheck.filter((el) => el === val).length <=
+          sol.filter((el) => el === val).length
+    )
+  );
+
+  if (ex.includes(true)) return true;
+
+  return false;
 };
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]));
